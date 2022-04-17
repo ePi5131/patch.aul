b4284ab5c5c3e39e92ce32f487c5a9e6c5062178
ePi <epi5131@gmail.com>
2022-04-17 10:07:23 +0900

    r20 設定ダイアログのサイズが変更されたときの画面外の領域が正しく描画されないのを修正

--- patch/patch_setting_dialog_wndproc_override.cpp
+++ patch/patch_setting_dialog_wndproc_override.cpp
@@ -1,5 +1,5 @@
 /*
-	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -13,25 +13,23 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#pragma once
-#include "macro.h"
-
-#include "global.hpp"
-#include "util_magic.hpp"
-
-#include "patch_setting_dialog_move.hpp"
+#include "patch_setting_dialog_wndproc_override.hpp"
 
 namespace patch {
-
-	inline class setting_dialog_t {
-		inline static WNDPROC wndproc_orig;
-		static LRESULT CALLBACK wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
-
-	public:
-		void operator()() {
-			wndproc_orig = reinterpret_cast<decltype(wndproc_orig)>(GLOBAL::exedit_base + 0x02cde0);
-			OverWriteOnProtectHelper h(GLOBAL::exedit_base + 0x02e804, 4);
-			h.store_i32(0, &wndproc);
+	LRESULT CALLBACK setting_dialog_t::wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
+		switch (message) {
+		case WM_MOVE:
+			#ifdef PATCH_SWITCH_SETTINGDIALOG_MOVE
+				setting_dialog_move(hwnd);
+			#endif
+			break;
+		case WM_SIZE:
+			#ifdef PATCH_SWITCH_FAST_SETTINGDIALOG
+				InvalidateRect(hwnd, nullptr, FALSE);
+				UpdateWindow(hwnd);
+			#endif
+			break;
 		}
-	} setting_dialog;
+		return wndproc_orig(hwnd, message, wparam, lparam);
+	}
 }
