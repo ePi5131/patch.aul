9529fab930695e480864e5f5728ea26949390fda
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-07-01 01:42:31 +0900

    アルファチャンネル合成バグの修正

--- patch/macro.h
+++ patch/macro.h
@@ -119,6 +119,7 @@
 #define PATCH_SWITCH_OBJ_LENSBLUR obj_lensblur
 #define PATCH_SWITCH_RCLICKMENU_SPLIT rclickmenu_split
 #define PATCH_SWITCH_RCLICKMENU_DELETE rclickmenu_delete
+#define PATCH_SWITCH_BLEND blend
 
 #define PATCH_SWITCH_UNDO undo
 #ifdef PATCH_SWITCH_UNDO
