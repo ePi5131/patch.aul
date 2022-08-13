998544a27e20bb07fb04e21eaea7cfe29d99ddb3
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-08-14 00:08:26 +0900

    sjis

--- patch/init.cpp
+++ patch/init.cpp
@@ -145,6 +145,10 @@ void init_t::InitAtExeditLoad() {
 	patch::helpful_msgbox.init();
 #endif
 
+#ifdef PATCH_SWITCH_FAILED_SJIS_MSGBOX
+	patch::failed_sjis_msgbox.init();
+#endif
+
 #ifdef PATCH_SWITCH_OBJ_LENSBLUR
 	patch::LensBlur.init();
 #endif
