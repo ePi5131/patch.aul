a2e47b17817b6d509f15c578b3dd293c85669f43
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-08-23 21:03:33 +0900

    add ext

--- patch/init.cpp
+++ patch/init.cpp
@@ -179,6 +179,9 @@ void init_t::InitAtExeditLoad() {
 #ifdef PATCH_SWITCH_BLEND
 	patch::blend.init();
 #endif
+#ifdef PATCH_SWITCH_ADD_EXTENSION
+	patch::add_extension.init();
+#endif
 	
 	patch::setting_dialog();
 
