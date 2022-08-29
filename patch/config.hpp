cfafa959f0dc69f74ee094c682c15b8a801d9d40
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-08-29 14:41:37 +0900

    scc

--- patch/config.hpp
+++ patch/config.hpp
@@ -128,6 +128,9 @@ public:
             #ifdef PATCH_SWITCH_OBJ_NOISE
                 patch::Noise.switch_load(cr);
             #endif
+            #ifdef PATCH_SWITCH_OBJ_SPECIALCOLORCONV
+                patch::obj_specialcolorconv.switch_load(cr);
+            #endif
             #ifdef PATCH_SWITCH_SETTINGDIALOG_EXCOLORCONFIG
                 patch::excolorconfig.switch_load(cr);
 		    #endif
@@ -411,6 +414,9 @@ public:
             #endif
             #ifdef PATCH_SWITCH_OBJ_NOISE
                 patch::Noise.switch_store(switch_);
+            #endif
+            #ifdef PATCH_SWITCH_OBJ_SPECIALCOLORCONV
+                patch::obj_specialcolorconv.switch_store(switch_);
             #endif
 		    #ifdef PATCH_SWITCH_SETTINGDIALOG_EXCOLORCONFIG
                 patch::excolorconfig.switch_store(switch_);
