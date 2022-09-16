b8136fec7deeac8769a9eb42b21198db6c6fab60
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-09-16 17:27:01 +0900

    Update config.hpp

--- patch/config.hpp
+++ patch/config.hpp
@@ -427,6 +427,9 @@ public:
             #ifdef PATCH_SWITCH_BLEND
                 patch::blend.switch_store(switch_);
             #endif
+            #ifdef PATCH_SWITCH_DIALOG_NEW_FILE
+                patch::dialog_new_file .switch_store(switch_);
+            #endif
 
 		    #ifdef PATCH_SWITCH_UNDO
                 patch::undo.switch_store(switch_);
