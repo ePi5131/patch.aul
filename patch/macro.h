c47fde37a55a51b9e746d90e24d8a6f6c2deeb1e
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-11-15 01:02:48 +0900

    newproject

--- patch/macro.h
+++ patch/macro.h
@@ -123,6 +123,7 @@
 #define PATCH_SWITCH_RCLICKMENU_SPLIT rclickmenu_split
 #define PATCH_SWITCH_RCLICKMENU_DELETE rclickmenu_delete
 #define PATCH_SWITCH_BLEND blend
+#define PATCH_SWITCH_SETTING_NEW_PROJECT setting_newproject
 
 #define PATCH_SWITCH_UNDO undo
 #ifdef PATCH_SWITCH_UNDO
