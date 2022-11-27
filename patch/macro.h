e3863b63af110d4266c3e1afa6f460c6cfdea749
ePi <epi5131@gmail.com>
2022-11-27 13:33:49 +0900

    r43beta1

--- patch/macro.h
+++ patch/macro.h
@@ -95,7 +95,7 @@
 #endif //define PATCH_SWITCH_FAST
 
 #else // ifdef PATCH_INTERNAL
-#define PATCH_VERSION_STR "r42"
+#define PATCH_VERSION_STR "r43 beta1"
 
 #define PATCH_SWITCH_EXCEPTION_LOG
 #define PATCH_SWITCH_SYSINFO_MODIFY
