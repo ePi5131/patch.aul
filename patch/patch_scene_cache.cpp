0dadc6b8252499598fe4c1649e88b7fdcd93c99f
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-11-27 20:47:42 +0900

    Add files via upload

---
+++ patch/patch_scene_cache.cpp
@@ -0,0 +1,65 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "patch_scene_cache.hpp"
+
+#ifdef PATCH_SWITCH_SCENE_CACHE
+namespace patch {
+
+	void* __cdecl scene_cache_t::get_scene_image_wrap(ExEdit::ObjectFilterIndex ofi, ExEdit::FilterProcInfo* efpip, int scene_idx, int frame, int subframe, int* w, int* h) {
+		int* SceneDisplaying = (int*)(GLOBAL::exedit_base + OFS::ExEdit::SceneDisplaying);
+		if (*SceneDisplaying != 0) {
+			return get_scene_image(ofi, efpip, scene_idx, frame, subframe, w, h);
+		}
+
+		auto a_exfunc = (AviUtl::ExFunc*)(GLOBAL::aviutl_base + OFS::AviUtl::exfunc);
+
+		void* smem_ptr = a_exfunc->get_shared_mem((int)&get_scene_image + scene_idx, (frame << 7) | subframe, NULL);
+		if (smem_ptr != NULL) {
+			reinterpret_cast<void(__cdecl*)(int, int*, int*, ExEdit::FilterProcInfo*)>(GLOBAL::exedit_base + OFS::ExEdit::get_scene_size)(scene_idx, w, h, efpip);
+			return smem_ptr;
+		}
+		int t0 = GetTickCount();
+		void* img_ptr = get_scene_image(ofi, efpip, scene_idx, frame, subframe, w, h);
+		if (img_ptr == NULL) {
+			return NULL;
+		}
+		if (64 < GetTickCount() - t0) {
+			int yc_size, flag;
+			if (reinterpret_cast<BOOL(__cdecl*)(int)>(GLOBAL::exedit_base + OFS::ExEdit::scene_has_alpha)(scene_idx)) {
+				yc_size = 8;
+				flag = 0x13000003;
+			} else {
+				yc_size = 6;
+				flag = 0x13000002;
+			}
+			void* smem_ptr = a_exfunc->create_shared_mem((int)&get_scene_image + scene_idx, (frame << 7) | subframe, efpip->scene_line * *h * yc_size, NULL);
+			if (smem_ptr == NULL) {
+				return img_ptr;
+			}
+			memcpy(smem_ptr, img_ptr, efpip->scene_line * *h * yc_size);
+		}
+		return img_ptr;
+	}
+	void scene_cache_t::delete_scene_cache() {
+		auto a_exfunc = (AviUtl::ExFunc*)(GLOBAL::aviutl_base + OFS::AviUtl::exfunc);
+		for (int i = 1; i < 50; i++) {
+			a_exfunc->delete_shared_mem((int)&get_scene_image + i, NULL);
+		}
+	}
+
+
+} // namespace patch
+#endif // ifdef PATCH_SWITCH_SCENE_CACHE
---
+++ patch/patch_scene_cache.hpp
@@ -0,0 +1,87 @@
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "macro.h"
+
+#ifdef PATCH_SWITCH_SCENE_CACHE
+
+#include <exedit.hpp>
+
+#include "global.hpp"
+#include "offset_address.hpp"
+#include "util.hpp"
+
+#include "config_rw.hpp"
+
+namespace patch {
+
+    // init at exedit load
+    // Rootで取得した場合のみシーンのキャッシュをとる
+
+    inline class scene_cache_t {
+
+        static void* __cdecl get_scene_image_wrap(ExEdit::ObjectFilterIndex ofi, ExEdit::FilterProcInfo* efpip, int scene_idx, int frame, int subframe, int* w, int* h);
+        static void delete_scene_cache();
+
+        inline static void* (__cdecl* get_scene_image)(ExEdit::ObjectFilterIndex, ExEdit::FilterProcInfo*, int, int, int, int*, int*);
+
+        bool enabled = true;
+        bool enabled_i;
+        inline static const char key[] = "scene_cache";
+    public:
+
+        void init() {
+            enabled_i = enabled;
+
+            if (!enabled_i)return;
+
+            get_scene_image = reinterpret_cast<decltype(get_scene_image)>(GLOBAL::exedit_base + OFS::ExEdit::get_scene_image);
+
+            {   // scene_obj
+                OverWriteOnProtectHelper h(GLOBAL::exedit_base + 0x0835bd, 4);
+                h.replaceNearJmp(0, &get_scene_image_wrap);
+            }
+            /*
+            {   // mask
+                OverWriteOnProtectHelper h(GLOBAL::exedit_base + 0x068a2d, 4);
+                h.replaceNearJmp(0, &get_scene_image_wrap);
+            }*/
+
+            {   // シーン切り替え時に初期化
+                OverWriteOnProtectHelper h(GLOBAL::exedit_base + 0x02be77, 5);
+                h.store_i8(0, '\xe9'); // jmp
+                h.replaceNearJmp(1, &delete_scene_cache);
+            }
+        }
+
+        void switching(bool flag) {
+            enabled = flag;
+        }
+
+        bool is_enabled() { return enabled; }
+        bool is_enabled_i() { return enabled_i; }
+
+        void switch_load(ConfigReader& cr) {
+            cr.regist(key, [this](json_value_s* value) {
+                ConfigReader::load_variable(value, enabled);
+                });
+        }
+
+        void switch_store(ConfigWriter& cw) {
+            cw.append(key, enabled);
+        }
+    } scene_cache;
+} // namespace patch
+
+#endif // ifdef PATCH_SWITCH_SCENE_CACHE
