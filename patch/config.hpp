6e76b865f50d6c8f8c3d350a9cace013ad0f80c7
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-12-09 18:23:10 +0900

    Update config.hpp

--- patch/config.hpp
+++ patch/config.hpp
@@ -173,6 +173,9 @@ public:
             #ifdef PATCH_SWITCH_BLEND
                 patch::blend.switch_load(cr);
             #endif
+            #ifdef PATCH_SWITCH_RENDERING
+                patch::Rendering.switch_load(cr);
+            #endif
             #ifdef PATCH_SWITCH_ADD_EXTENSION
                 patch::add_extension.switch_load(cr);
             #endif
@@ -501,6 +504,9 @@ public:
             #endif
             #ifdef PATCH_SWITCH_BLEND
                 patch::blend.switch_store(switch_);
+            #endif
+            #ifdef PATCH_SWITCH_RENDERING
+                patch::Rendering.switch_store(switch_);
             #endif
             #ifdef PATCH_SWITCH_ADD_EXTENSION
                 patch::add_extension.switch_store(switch_);
